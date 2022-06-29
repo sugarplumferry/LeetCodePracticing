226d4dedbf03aedaed15e0c91c5b98fc01b2094e
Charlie <charlietsai@ptc-nec.com.tw>
2022-06-29 13:30:03 +0800

    Solve merge two sorted list by two pointer parameter
    Signed-off-by: Charlie <charlietsai@ptc-nec.com.tw>

---
+++ MergeTwoSortedLists.js
@@ -0,0 +1,44 @@
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+function ListNode(val, next) {
+	this.val = val === undefined ? 0 : val;
+	this.next = next === undefined ? null : next;
+}
+
+var mergeTwoLists = function (list1, list2) {
+	let head = new ListNode();
+	let result = head;
+	while (list1 && list2) {
+		if (list1.val >= list2.val) {
+			head.next = list2;
+			list2 = list2.next;
+		} else {
+			head.next = list1;
+			list1 = list1.next;
+		}
+
+		head = head.next;
+	}
+	head.next = list1 || list2;
+
+	return result.next;
+};
+
+var GenerateLinkedList = function (array) {
+	let list = new ListNode();
+	let head = list;
+	for (let i = 0; i < array.length; i++) {
+		list.val = array[i];
+		list.next = new ListNode();
+		list = list.next;
+	}
+	return head;
+};
+
+let list1 = GenerateLinkedList([1, 2, 4]),
+	list2 = GenerateLinkedList([1, 3, 4]);
+
+console.log(mergeTwoLists(list1, list2));
